773191453814c7aea92086e0591c7b08024dae0b
Dylan B <dylanrb@live.com>
2015-10-05 15:26:05 -0600

    App as completely as it will be and addition of UML

--- Drblake-Reflex/app/src/main/java/cs/ualberta/ca/drblake_reflex/Save_And_Load.java
+++ Drblake-Reflex/app/src/main/java/cs/ualberta/ca/drblake_reflex/Save_And_Load.java
@@ -1,17 +1,39 @@
+/*This file is part of Drblake-reflex.
+
+    Drblake-reflex is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Drblake-reflex is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Drblake-reflex.  If not, see <http://www.gnu.org/licenses/>.*/
 package cs.ualberta.ca.drblake_reflex;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 /**
  * Created by Dylan on 2015-10-04.
  */
 public class Save_And_Load {
-    public void SaveWinner(String winner,int gameMode){
+    public void saveWinner(String winner,int gameMode){
+        if (gameMode == 2){
+            //Store in list 2
+        }
+        if (gameMode == 3){
+            //Store in list 3
+        }
+        if (gameMode == 4){
+            //Store in list 4
+        }
 
     }
-    public void SaveReaction(long reactionTime){
-
+    public void saveReaction(long reactionTime){
+        //store in list 1
     }
     public ArrayList loadReaction(){
         //return arraylist of reaction times
--- Drblake-Reflex/app/src/main/java/cs/ualberta/ca/drblake_reflex/Timer.java
+++ Drblake-Reflex/app/src/main/java/cs/ualberta/ca/drblake_reflex/Timer.java
@@ -1,7 +1,20 @@
+/*This file is part of Drblake-reflex.
+
+    Drblake-reflex is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Drblake-reflex is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Drblake-reflex.  If not, see <http://www.gnu.org/licenses/>.*/
 package cs.ualberta.ca.drblake_reflex;
 
 import java.util.Random;
-import android.os.SystemClock;
 /**
  * Created by Dylan on 2015-10-04.
  */
@@ -17,11 +30,13 @@ public class Timer{
 
     }
     public void Reset(){
+
         start();
     }
+
     public long ReactionTime() {
         Save_And_Load s = new Save_And_Load();
-        s.SaveReaction(System.currentTimeMillis()-time);
+        s.saveReaction(System.currentTimeMillis()-time);
         return (System.currentTimeMillis()-time);
     }
     public long RandomTime(){
